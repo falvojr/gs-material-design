2aa481e220c987b0aab0ab1572fa3656a15053e0
Venilton FalvoJr <falvojr@gmail.com>
2015-08-23 14:20:40 -0300

    add actions to toolbar/actionbar/appbar + navigation

--- app/src/main/java/com/falvojr/material/SubActivity.java
+++ app/src/main/java/com/falvojr/material/SubActivity.java
@@ -1,30 +1,26 @@
 package com.falvojr.material;
 
-import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Toolbar mToolbar;
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
-        mToolbar = (Toolbar) super.findViewById(R.id.app_bar);
-        super.setSupportActionBar(mToolbar);
-    }
+        Toolbar toolbar = (Toolbar) super.findViewById(R.id.app_bar);
+        super.setSupportActionBar(toolbar);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        assert super.getSupportActionBar() != null : "Support ActionBar is null";
+
+        super.getSupportActionBar().setHomeButtonEnabled(true);
+        super.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -33,10 +29,9 @@ public class MainActivity extends AppCompatActivity {
         // the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.action_navigate) {
-            super.startActivity(new Intent(this, SubActivity.class));
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
